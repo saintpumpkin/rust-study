be676b60a43130333b12b8ff5ce45fb6b95de498
Your Name <you@example.com>
2023-08-04 19:09:22 +0900

    Week4 Init

---
+++ src/week4.rs
@@ -0,0 +1,313 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::collections::HashSet;
+use std::mem::transmute;
+
+fn if_expression() {
+    let mut x = 10;
+    if x % 2 == 0 {
+        x = x / 2;
+    } else {
+        x = 3 * x + 1;
+    }
+}
+
+fn if_let_expression() {
+    let arg = std::env::args().next();
+    if let Some(value) = arg {
+        println!("Program name: {value}");
+    } else {
+        println!("Missing name?");
+    }
+}
+
+fn while_loop() {
+    let mut x = 10;
+    while x != 1 {
+        x = if x % 2 == 0 {
+            x / 2
+        } else {
+            3 * x + 1
+        };
+    }
+    println!("Final x: {x}");
+}
+
+fn while_let_loop() {
+    let v = vec![10, 20, 30];
+    let mut iter = v.into_iter();
+
+    while let Some(x) = iter.next() {
+        println!("x: {x}");
+    }
+}
+
+fn for_loop() {
+    let v = vec![10, 20, 30];
+
+    for x in v {
+        println!("x: {x}");
+    }
+    
+    for i in (0..10).step_by(2) {
+        println!("i: {i}");
+    }
+}
+
+fn loop_expression() {
+    let mut x = 10;
+    loop {
+        x = if x % 2 == 0 {
+            x / 2
+        } else {
+            3 * x + 1
+        };
+        if x == 1 {
+            break;
+        }
+    }
+    println!("Final x: {x}");
+}
+
+fn match_expression() {
+    match std::env::args().next().as_deref() {
+        Some("cat") => println!("Will do cat things"),
+        Some("ls")  => println!("Will ls some files"),
+        Some("mv")  => println!("Let's move some files"),
+        Some("rm")  => println!("Uh, dangerous!"),
+        None        => println!("Hmm, no program name?"),
+        _           => println!("Unknown program name!"),
+    }
+}
+
+fn break_and_continue() {
+    let v = vec![10, 20, 30];
+    let mut iter = v.into_iter();
+    'outer: while let Some(x) = iter.next() {
+        println!("x: {x}");
+        let mut i = 0;
+        while i < x {
+            println!("x: {x}, i: {i}");
+            i += 1;
+            if i == 3 {
+                break 'outer;
+            }
+        }
+    }
+}
+
+fn option_and_result() {
+    let numbers = vec![10, 20, 30];
+    let first: Option<&i8> = numbers.first();
+    println!("first: {first:?}");
+
+    let idx: Result<usize, usize> = numbers.binary_search(&10);
+    println!("idx: {idx:?}");
+}
+
+fn string_string() {
+    let mut s1 = String::new();
+    s1.push_str("Hello");
+    println!("s1: len = {}, capacity = {}", s1.len(), s1.capacity());
+
+    let mut s2 = String::with_capacity(s1.len() + 1);
+    s2.push_str(&s1);
+    s2.push('!');
+    println!("s2: len = {}, capacity = {}", s2.len(), s2.capacity());
+
+    let s3 = String::from("🇨🇭");
+    println!("s3: len = {}, number of chars = {}", s3.len(),
+             s3.chars().count());
+}
+
+fn vec_vec() {
+    let mut v1 = Vec::new();
+    v1.push(42);
+    println!("v1: len = {}, capacity = {}", v1.len(), v1.capacity());
+
+    let mut v2 = Vec::with_capacity(v1.len() + 1);
+    v2.extend(v1.iter());
+    v2.push(9999);
+    println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
+
+    // 요소로 벡터를 초기화하는 표준 매크로입니다.
+    let mut v3 = vec![0, 0, 1, 2, 3, 4];
+
+    // 짝수 요소만 유지합니다.
+    v3.retain(|x| x % 2 == 0);
+    println!("{v3:?}");
+
+    // 연속 중복을 삭제합니다.
+    v3.dedup();
+    println!("{v3:?}");
+}
+
+fn hash_map() {
+    let mut page_counts = HashMap::new();
+    page_counts.insert("Adventures of Huckleberry Finn".to_string(), 207);
+    page_counts.insert("Grimms' Fairy Tales".to_string(), 751);
+    page_counts.insert("Pride and Prejudice".to_string(), 303);
+
+    if !page_counts.contains_key("Les Misérables") {
+        println!("We know about {} books, but not Les Misérables.",
+                 page_counts.len());
+    }
+
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        match page_counts.get(book) {
+            Some(count) => println!("{book}: {count} pages"),
+            None => println!("{book} is unknown.")
+        }
+    }
+
+    // 아무것도 찾을 수 없는 경우 .entry() 메서드를 사용하여 값을 삽입합니다.
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        let page_count: &mut i32 = page_counts.entry(book.to_string()).or_insert(0);
+        *page_count += 1;
+    }
+
+    println!("{page_counts:#?}");
+}
+
+fn box_box() {
+    let five = Box::new(5);
+    println!("five: {}", *five);
+}
+
+#[derive(Debug)]
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
+}
+
+fn box_in_recursive_data_structure() {
+    let list: List<i32> = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+    println!("{list:?}");
+}
+
+fn niche_optimization() {
+    let list: List<i32> = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+    println!("{list:?}");
+}
+
+fn rc_rc() {
+    let mut a = Rc::new(10);
+    let mut b = Rc::clone(&a);
+
+    println!("a: {a}");
+    println!("b: {b}");
+}
+
+mod foo {
+    pub fn do_something() {
+        println!("In the foo module");
+    }
+}
+
+mod bar {
+    pub fn do_something() {
+        println!("In the bar module");
+    }
+}
+
+fn module_module() {
+    foo::do_something();
+    bar::do_something();
+}
+
+mod outer {
+    fn private() {
+        println!("outer::private");
+    }
+
+    pub fn public() {
+        println!("outer::public");
+    }
+
+    mod inner {
+        fn private() {
+            println!("outer::inner::private");
+        }
+
+        pub fn public() {
+            println!("outer::inner::public");
+            super::private();
+        }
+    }
+}
+
+fn visibility() {
+    outer::public();
+}
+
+pub fn luhn(cc_number: &str) -> bool {
+    let trim_str = cc_number.trim();
+    if cc_number.is_empty() {
+        return false;
+    }
+    if cc_number.len() < 2 {
+        return false;
+    }
+    // for number in cc_number {
+    //     number
+    // }
+    return false;
+}
+
+#[test]
+fn test_non_digit_cc_number() {
+    assert!(!luhn("foo"));
+}
+
+#[test]
+fn test_empty_cc_number() {
+    assert!(!luhn(""));
+    assert!(!luhn(" "));
+    assert!(!luhn("  "));
+    assert!(!luhn("    "));
+}
+
+#[test]
+fn test_single_digit_cc_number() {
+    assert!(!luhn("0"));
+}
+
+#[test]
+fn test_two_digit_cc_number() {
+    assert!(luhn(" 0 0 "));
+}
+
+#[test]
+fn test_valid_cc_number() {
+    assert!(luhn("4263 9826 4026 9299"));
+    assert!(luhn("4539 3195 0343 6467"));
+    assert!(luhn("7992 7398 713"));
+}
+
+#[test]
+fn test_invalid_cc_number() {
+    assert!(!luhn("4223 9826 4026 9299"));
+    assert!(!luhn("4539 3195 0343 6476"));
+    assert!(!luhn("8273 1232 7352 0569"));
+}
+
+pub fn week4() {
+    if_expression();
+    if_let_expression();
+    while_loop();
+    while_let_loop();
+    for_loop();
+    loop_expression();
+    match_expression();
+    break_and_continue();
+    option_and_result();
+    string_string();
+    vec_vec();
+    hash_map();
+    box_box();
+    box_in_recursive_data_structure();
+    niche_optimization();
+    rc_rc();
+    module_module();
+    visibility();
+}
