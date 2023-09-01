97278dcf5cf2353edbe6013af246759471e9986c
Your Name <you@example.com>
2023-09-01 19:23:01 +0900

    Week8 studying...

--- src/week8.rs
+++ src/week8.rs
@@ -1,9 +1,24 @@
+#![allow(dead_code)]
+
 use std::pin::Pin;
 use std::task::Context;
 use futures::executor::block_on;
 use tokio::time;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
+use tokio::sync::mpsc::{self, Receiver};
+use anyhow::Result;
+use futures::future;
+use reqwest;
+use std::collections::HashMap;
+use tokio::time::{sleep, Duration};
+use futures::future::join_all;
+use std::time::Instant;
+use tokio::sync::{oneshot};
+use tokio::task::spawn;
+use async_trait::async_trait;
+use std::io::{ErrorKind};
+use tokio::io::{DuplexStream};
 
 async fn count_to(count: i32) {
     for i in 1..=count {
@@ -29,7 +44,7 @@ pub enum Poll<T> {
     Pending,
 }
 
-async fn count_to2(count: i32) {
+pub async fn count_to2(count: i32) {
     for i in 1..=count {
         println!("Count in task: {i}!");
         time::sleep(time::Duration::from_millis(5)).await;
@@ -79,6 +94,248 @@ async fn main2() -> io::Result<()> {
             }
         });
     }
+}
+
+
+pub async fn ping_handler(mut input: Receiver<()>) {
+    let mut count: usize = 0;
+
+    while let Some(_) = input.recv().await {
+        count += 1;
+        println!("Received {count} pings so far.");
+    }
+
+    println!("ping_handler complete");
+}
+
+#[tokio::main]
+async fn main3() {
+    let (sender, receiver) = mpsc::channel(32);
+    let ping_handler_task = tokio::spawn(ping_handler(receiver));
+    for i in 0..10 {
+        sender.send(()).await.expect("Failed to send ping.");
+        println!("Sent {} pings so far.", i + 1);
+    }
+
+    drop(sender);
+    ping_handler_task.await.expect("Something went wrong in ping handler task.");
+}
+
+pub async fn size_of_page(url: &str) -> Result<usize> {
+    let resp = reqwest::get(url).await?;
+    Ok(resp.text().await?.len())
+}
+
+#[tokio::main]
+async fn main4() {
+    let urls: [&str; 4] = [
+        "https://google.com",
+        "https://httpbin.org/ip",
+        "https://play.rust-lang.org/",
+        "BAD_URL",
+    ];
+    let futures_iter = urls.into_iter().map(size_of_page);
+    let results = future::join_all(futures_iter).await;
+    let page_sizes_dict: HashMap<&str, Result<usize>> =
+        urls.into_iter().zip(results.into_iter()).collect();
+    println!("{:?}", page_sizes_dict);
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Animal {
+    Cat { name: String },
+    Dog { name: String },
+}
+
+pub async fn first_animal_to_finish_race(
+    mut cat_rcv: Receiver<String>,
+    mut dog_rcv: Receiver<String>,
+) -> Option<Animal> {
+    tokio::select! {
+        cat_name = cat_rcv.recv() => Some(Animal::Cat { name: cat_name? }),
+        dog_name = dog_rcv.recv() => Some(Animal::Dog { name: dog_name? })
+    }
+}
+
+#[tokio::main]
+async fn main5() {
+    let (cat_sender, cat_receiver) = mpsc::channel(32);
+    let (dog_sender, dog_receiver) = mpsc::channel(32);
+    tokio::spawn(async move {
+        sleep(Duration::from_millis(500)).await;
+        cat_sender
+            .send(String::from("Felix"))
+            .await
+            .expect("Failed to send cat.");
+    });
+    tokio::spawn(async move {
+        sleep(Duration::from_millis(50)).await;
+        dog_sender
+            .send(String::from("Rex"))
+            .await
+            .expect("Failed to send dog.");
+    });
+
+    let winner = first_animal_to_finish_race(cat_receiver, dog_receiver)
+        .await
+        .expect("Failed to receive winner");
+
+    println!("Winner is {winner:?}");
+}
+
+pub async fn sleep_ms(start: &Instant, id: u64, duration_ms: u64) {
+    std::thread::sleep(std::time::Duration::from_millis(duration_ms));
+    println!(
+        "future {id} slept for {duration_ms}ms, finished after {}ms",
+        start.elapsed().as_millis()
+    );
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main6() {
+    let start = Instant::now();
+    let sleep_futures = (1..=10).map(|t| sleep_ms(&start, t, t * 10));
+    join_all(sleep_futures).await;
+}
+
+// 작업 항목. 이 경우 지정된 시간 동안 절전 모드로 있다가
+// `respond_on` 채널의 메시지로 응답합니다.
+#[derive(Debug)]
+pub struct Work {
+    input: u32,
+    respond_on: oneshot::Sender<u32>,
+}
+
+// 대기열에서 작업을 리슨하고 실행하는 worker입니다.
+pub async fn worker(mut work_queue: mpsc::Receiver<Work>) {
+    let mut iterations = 0;
+    loop {
+        tokio::select! {
+            Some(work) = work_queue.recv() => {
+                sleep(Duration::from_millis(10)).await; // Pretend to work.
+                work.respond_on
+                    .send(work.input * 1000)
+                    .expect("failed to send response");
+                iterations += 1;
+            }
+            // TODO: 100ms마다 반복 횟수를 보고합니다.
+        }
+    }
+}
+
+// 작업을 요청하고 작업이 완료될 때까지 기다리는 요청자입니다.
+pub async fn do_work(work_queue: &mpsc::Sender<Work>, input: u32) -> u32 {
+    let (tx, rx) = oneshot::channel();
+    work_queue
+        .send(Work {
+            input,
+            respond_on: tx,
+        })
+        .await
+        .expect("failed to send on work queue");
+    rx.await.expect("failed waiting for response")
+}
+
+#[tokio::main]
+async fn main7() {
+    let (tx, rx) = mpsc::channel(10);
+    spawn(worker(rx));
+    for i in 0..100 {
+        let resp = do_work(&tx, i).await;
+        println!("work result for iteration {i}: {resp}");
+    }
+}
+
+#[async_trait]
+pub trait Sleeper {
+    async fn sleep(&self);
+}
+
+pub struct FixedSleeper {
+    pub sleep_ms: u64,
+}
+
+#[async_trait]
+impl Sleeper for FixedSleeper {
+    async fn sleep(&self) {
+        sleep(Duration::from_millis(self.sleep_ms)).await;
+    }
+}
+
+pub async fn run_all_sleepers_multiple_times(sleepers: Vec<Box<dyn Sleeper>>, n_times: usize) {
+    for _ in 0..n_times {
+        println!("running all sleepers..");
+        for sleeper in &sleepers {
+            let start = Instant::now();
+            sleeper.sleep().await;
+            println!("slept for {}ms", start.elapsed().as_millis());
+        }
+    }
+}
+
+#[tokio::main]
+async fn main8() {
+    let sleepers: Vec<Box<dyn Sleeper>> = vec![
+        Box::new(FixedSleeper { sleep_ms: 50 }),
+        Box::new(FixedSleeper { sleep_ms: 100 }),
+    ];
+    run_all_sleepers_multiple_times(sleepers, 5).await;
+}
+
+pub struct LinesReader {
+    stream: DuplexStream,
+}
+
+impl LinesReader {
+    pub fn new(stream: DuplexStream) -> Self {
+        Self { stream }
+    }
+
+    pub async fn next(&mut self) -> io::Result<Option<String>> {
+        let mut bytes = Vec::new();
+        let mut buf = [0];
+        while self.stream.read(&mut buf[..]).await? != 0 {
+            bytes.push(buf[0]);
+            if buf[0] == b'\n' {
+                break;
+            }
+        }
+        if bytes.is_empty() {
+            return Ok(None)
+        }
+        let s = String::from_utf8(bytes)
+            .map_err(|_| io::Error::new(ErrorKind::InvalidData, "not UTF-8"))?;
+        Ok(Some(s))
+    }
+}
+
+pub async fn slow_copy(source: String, mut dest: DuplexStream) -> std::io::Result<()> {
+    for b in source.bytes() {
+        dest.write_u8(b).await?;
+        tokio::time::sleep(Duration::from_millis(10)).await
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main9() -> std::io::Result<()> {
+    let (client, server) = tokio::io::duplex(5);
+    let handle = tokio::spawn(slow_copy("hi\nthere\n".to_owned(), client));
+
+    let mut lines = LinesReader::new(server);
+    let mut interval = tokio::time::interval(Duration::from_millis(60));
+    loop {
+        tokio::select! {
+            _ = interval.tick() => println!("tick!"),
+            line = lines.next() => if let Some(l) = line? {
+                print!("{}", l)
+            } else {
+                break
+            },
+        }
+    }
+    handle.await.unwrap()?;
+    Ok(())
 }
 
 pub fn week8() {
