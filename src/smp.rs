d8d25d77860df1f763208934a4c6d2606bb56ccd
박성호 <katarn30@gmail.com>
2023-09-14 21:19:27 +0900

    ping pong 1

--- src/smp.rs
+++ src/smp.rs
@@ -11,33 +11,63 @@ use crossterm::{
 pub struct SmpController {
     x: usize,
     y: usize,
+    width: usize,
+    height: usize,
+    add_value: i32,
 }
 
 impl SmpController {
-    pub fn new() -> SmpController {
+    pub fn new(width: usize, height: usize) -> SmpController {
         SmpController {
             x: 0,
             y: 0,
+            width: width,
+            height: height,
+            add_value: 2,
         }
     }
 
     pub fn move_up(&mut self) {
-        self.y = std::cmp::max(0, self.y - 1);
+        self.y = match self.y.checked_sub(1) {
+            Some(y) => y,
+            None => 0,
+        };
     }
 
     pub fn move_down(&mut self) {
-        self.y = self.y + 1;
+        self.y = (self.y + 1).clamp(0, self.height);
+        if (self.height < self.y) {
+            println!("overflow!!!!!");
+        }
     }
 
     pub fn move_right(&mut self) {
-        self.x = self.x + 1;
+        self.x = (self.x + 2).clamp(0, self.width);
+        if (self.width < self.x) {
+            println!("overflow!!!!!");
+        }
     }
 
     pub fn move_left(&mut self) {
-        self.x = std::cmp::max(0, self.x - 1);
+        self.x = match self.x.checked_sub(2) {
+            Some(x) => x,
+            None => 0,
+        };
     }
 
-    pub fn update(&self, render: &mut SmpRender) {
+    fn auto_movement(&mut self) {
+        let mut cur_x: i32 = self.x as i32;
+        if cur_x == 70 {
+            self.add_value = -2;
+        } else if cur_x == 0{
+            self.add_value = 2;
+        }
+        cur_x = cur_x + self.add_value;
+        self.x = cur_x as usize;
+    }
+
+    pub fn update(&mut self, render: &mut SmpRender) {
+        self.auto_movement();
         render.set_object_position(self.x, self.y)
     }
 }
@@ -50,10 +80,11 @@ pub struct Smp {
 
 impl Smp {
     fn new() -> Smp {
+        let render = SmpRender::new();
         Smp {
-            controller: SmpController::new(),
+            controller: SmpController::new(render.get_width(), render.get_height()),
             input: SmpInput::<SmpController>::new(),
-            render: SmpRender::new(),
+            render: render,
         }
     }
 
@@ -70,7 +101,7 @@ impl Smp {
             self.input.update(&mut self.controller);
             self.controller.update(&mut self.render);
             self.render.render();
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_millis(10));
         }
     }
 
--- src/smp_render.rs
+++ src/smp_render.rs
@@ -30,29 +30,41 @@ fn write_chars() -> io::Result<()> {
 pub struct SmpRender {
     x: usize,
     y: usize,
+    prev_x: usize,
+    prev_y: usize,
     width: usize,
-    heigth: usize,
+    height: usize,
     //buffer: Vec<char>
 }
 
 impl SmpRender {
     pub fn new() -> SmpRender {
         let term = Term::stdout();
-        let (heigth, width) = term.size();
+        let (height, width) = term.size();
         SmpRender {
             x: 0,
             y: 0,
+            prev_x: 0,
+            prev_y: 0,
             width: width as usize,
-            heigth: heigth as usize,
+            height: height as usize,
             //buffer: Vec::<char>::new(),
         }
+    }
+
+    pub fn get_width(&self) -> usize {
+        self.width
+    }
+
+    pub fn get_height(&self) -> usize {
+        self.height
     }
 
     fn draw_background(&mut self) {
         //self.buffer.resize(self.width * self.heigth, '■');
         let term = Term::stdout();
         for x in 0..self.width {
-            for y in 0..self.heigth {
+            for y in 0..self.height {
                 term.move_cursor_to(x as usize, y as usize);
                 let text = format!("{}", style('■').black().on_red());
                 term.write_str(&text);
@@ -60,10 +72,10 @@ impl SmpRender {
         }
     }
 
-    fn draw_object(&mut self, x: usize, y: usize) {
+    fn draw_object(&mut self, x: usize, y: usize, text: String) {
         let term = Term::stdout();
         term.move_cursor_to(x, y);
-        let text = format!("{}", style('○').black().on_green());
+        //let text = format!("{}", style('○').black().on_green());
         term.write_str(&text);
     }
 
@@ -75,7 +87,10 @@ impl SmpRender {
     pub fn render(&mut self) {
         //let term = Term::stdout();
         //self.draw_background();
-        self.draw_object(self.x, self.y);
+        self.draw_object(self.prev_x, self.prev_y, format!("{}", style("  ").black()));
+        self.draw_object(self.x, self.y, format!("{}", style('○').black().on_green()));
+        self.prev_x = self.x;
+        self.prev_y = self.y;
         //let string = self.buffer.iter().collect::<String>();
         //term.write_str(&string);
     }
